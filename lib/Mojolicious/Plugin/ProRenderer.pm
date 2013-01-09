492d4649570b334d5fae3e926c1ab445494e679c
Sergey Lobin <ifitwasi@gmail.com>
2013-01-09 22:07:53 +0400

    Added support of templates in __DATA__ section

--- lib/Mojolicious/Plugin/ProRenderer.pm
+++ lib/Mojolicious/Plugin/ProRenderer.pm
@@ -1,6 +1,7 @@
 package Mojolicious::Plugin::ProRenderer;
+
 {
-  $Mojolicious::Plugin::ProRenderer::VERSION = '0.40';
+  $Mojolicious::Plugin::HtpRenderer::VERSION = '0.40';
 }
 
 use strict;
@@ -103,6 +104,8 @@ sub _render {
     return 1;
 }
 
+1;  
+
 =head1 NAME
 
 Mojolicious::Plugin::ProRenderer - HTML::Tempate::Pro render plugin for Mojolicious
@@ -115,10 +118,11 @@ Version 0.40
 
 Add the handler:
 
-    *Mojolicious*
+    # Mojolicious
   
     sub startup {
        ...
+
        $self->plugin('pro_renderer');
        ...
     }
@@ -127,27 +131,8 @@ Add the handler:
 
     $self->render(
         message => 'test', 
-        list    => [
-          { id => 1 }, 
-          { id => 2 }
-        ]
+        list    => [{id => 1}, { id=>2 }]
     );
-    
-    *Mojolicious::Lite*
-    ...
-    plugin pro_renderer; 
-    ...
-    # or with options:
-    plugin pro_renderer => {
-	  template_options => {
-	    debug => 1,
-	    ...
-	  }
-    };
-    
-=head2 OPTIONS
-
-    options are the same as in HTML::Template::Pro constructor, added only debug
 
 =head1 FUNCTIONS
 
@@ -159,90 +144,4 @@ create handler for renderer
 
 register plugin 'pro_renderer'
 
-=head1 AUTHOR
-
-Sergey Lobin, C<< <ifitwasi at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-mojolicious-plugin-prorenderer at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mojolicious-Plugin-ProRenderer>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Mojolicious::Plugin::ProRenderer
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Mojolicious-Plugin-ProRenderer>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Mojolicious-Plugin-ProRenderer>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Mojolicious-Plugin-ProRenderer>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Mojolicious-Plugin-ProRenderer/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Sergey Lobin.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 =cut
-
-1; # End of Mojolicious::Plugin::ProRenderer
